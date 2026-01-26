c24e60c1569f778bf89f8f68ba406179b141e5ea
alejandrogzi <alejandrxgzi@gmail.com>
2026-01-26 16:24:36 +0100

    chore: drops

--- modules/blast/src/consts.rs
+++
@@ -1,5 +0,0 @@
-pub const RESULT: &str = "result";
-pub const VENV: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/.venv/bin/activate");
-pub const ORF_PEP: &str = "orf.pep";
-
-pub const SCALE: u64 = 100_000_000_000; // 100Gb
